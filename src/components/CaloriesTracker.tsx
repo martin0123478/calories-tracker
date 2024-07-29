7f6a0ad997e7de72b47a05f3e15946c935801c7d
martin0123478 <martin_agui97@outlook.es>
2024-07-29 12:52:16 -0600

    feat:show diference calories totals

--- src/components/CaloriesTracker.tsx
+++ src/components/CaloriesTracker.tsx
@@ -11,6 +11,8 @@ export const CaloriesTracker = ({ activities }: CaloriesTrackerProps) => {
     const caloriesConsumed = useMemo(() => activities.reduce((total, activity) => activity.category === 1 ? total + activity.calories : total, 0), [activities])
 
     const caloriesburned = useMemo(() => activities.reduce((total, activity) => activity.category === 2 ? total + activity.calories : total, 0), [activities])
+
+    const netCalories = useMemo(() => caloriesConsumed - caloriesburned, [activities])
     return (
         <>
             <h2 className="text-4xl font-black text-white text-center">Resumen de calorias</h2>
@@ -23,6 +25,11 @@ export const CaloriesTracker = ({ activities }: CaloriesTrackerProps) => {
                 <Calories
                     calories={caloriesburned}
                     text="Quemadas"
+                />
+
+                <Calories
+                    calories={netCalories}
+                    text="Diferencia"
                 />
             </div>
 
